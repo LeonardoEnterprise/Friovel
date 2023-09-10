0411579ab4ed4225bdd88b0a90fcbb3f0b745d89
LeticiaGiacomini <giacominileticia709@gmail.com>
2023-09-10 13:55:53 -0300

    CONSTRUCAO/ final campo faq

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isHomeRoute: boolean | undefined;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.isHomeRoute = this.router.url === '/home';
+    });
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -73,14 +73,15 @@ export class HomeComponent {
   }
 
   faqList = [
-    { question: 'O que é Angular?', answer: 'Angular é um framework de desenvolvimento web...', showAnswer: false  },
-    { question: 'Como começar a usar Angular?', answer: 'Para começar com Angular, você precisa...', showAnswer: false  },
-    { question: 'Qual a versão mais recente do Angular?', answer: 'A versão mais recente do Angular é...', showAnswer: false  },
+    { question: 'O que é Angular?', answer: 'Angular é um framework de desenvolvimento web...', showAnswer: false, isClicked: false  },
+    { question: 'Como começar a usar Angular?', answer: 'Para começar com Angular, você precisa...', showAnswer: false, isClicked: false  },
+    { question: 'Qual a versão mais recente do Angular?', answer: 'A versão mais recente do Angular é...', showAnswer: false, isClicked: false  },
     // Adicione mais perguntas e respostas aqui
   ];
 
-  toggleAnswer(question: FaqQuestion) {
+  toggleAnswer(question: { showAnswer: boolean; isClicked: boolean; }) {
     question.showAnswer = !question.showAnswer;
+    question.isClicked = !question.isClicked;
   }
 }
 
